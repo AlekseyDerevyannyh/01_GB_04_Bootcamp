d23cb57bf8a82a028ca36489b6edf9e4bceba967
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-09-06 19:17:37 +0300

    add templates in Flask

--- 15_Flask/main.py
+++ 15_Flask/main.py
@@ -1,14 +1,15 @@
 # pip install flask
 from os import system
-from flask import Flask		# Вытащил объект 
+from flask import Flask, render_template	# Вытащил объект 
 
 system("cls")
 
-app = Flask(__name__)		# Создали веб приложение
+app = Flask(__name__)	# Создали веб приложение
 
-@app.route('/<x>/<y>')		# Создание главной страницы
-def main(x, y):
-	return str(int(x) + int(y))
+@app.route('/<name>')	# Создание главной страницы
+def main(name):
+	title = 'Igor'
+	return render_template('base.html', name = name, title = 'BootCamp')
 
 if __name__ == '__main__':
 	app.run()
