cfc650eaff27e2a57ae44e6bf9c42487f4d5c442
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-09-06 19:52:27 +0300

    add generating pages in Flask project

--- 15_Flask/main.py
+++ 15_Flask/main.py
@@ -9,7 +9,11 @@ app = Flask(__name__)	# Создали веб приложение
 @app.route('/')	# Создание главной страницы
 def main():
 	user_name = ['Aleksey', 'Alla', 'Almir', 'Diana', 'Ilali']
-	return render_template('base.html', user_name = user_name, title = 'BootCamp')
+	return render_template('base.html', user_name = user_name, title = 'Главная страница')
+
+@app.route('/bootcamp')
+def boot():
+	return render_template('bootcamp.html', title = 'BootCamp-tasks')
 
 if __name__ == '__main__':
 	app.run()
