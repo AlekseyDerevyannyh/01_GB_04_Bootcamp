383ac47ed82b3ca37a10829007cfe63d2e45a152
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-09-25 15:52:51 +0300

    add in Task 15 Post Get and Form

--- 15_Flask/main.py
+++ 15_Flask/main.py
@@ -1,19 +1,26 @@
 # pip install flask
+# pip install flask-wtf
+# pip install email_validator
+
 from os import system
 from flask import Flask, render_template	# Вытащил объект 
+from auth import LoginForm
 
 system("cls")
 
 app = Flask(__name__)	# Создали веб приложение
+app.config['SECRET_KEY'] = 'world world world hello'	#csrf атаки
 
-@app.route('/')	# Создание главной страницы
+@app.route('/', methods=['GET', 'POST'])	# Создание главной страницы
 def main():
-	user_name = ['Aleksey', 'Alla', 'Almir', 'Diana', 'Ilali']
-	return render_template('base.html', user_name = user_name, title = 'Главная страница')
-
-@app.route('/bootcamp')
-def boot():
-	return render_template('bootcamp.html', title = 'BootCamp-tasks')
+	form = LoginForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		surname = form.surname.data
+		email = form.email.data
+		print(name, surname, email)
+		return render_template('base.html', title = 'Главная страница', message = 'Вы авторизовались')
+	return render_template('base.html', title = 'Главная страница', form = form)
 
 if __name__ == '__main__':
 	app.run()
